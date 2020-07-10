afcdaa48d6550f4093d4448048739ec1d504436a
Guy Roberts <guyroberts21@outlook.com>
2020-07-10 14:57:53 +0100

    Updates

---
+++ precache-manifest.4e926d900965709f24371af1b3cf79fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a139d21da152ae8f3ed000edc80f6a9d",
+    "url": "/react-library/index.html"
+  },
+  {
+    "revision": "8fe60d14e4c1400f90e6",
+    "url": "/react-library/static/css/main.bd292220.chunk.css"
+  },
+  {
+    "revision": "0ee368effd35e0eeda7f",
+    "url": "/react-library/static/js/2.0de25585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-library/static/js/2.0de25585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe60d14e4c1400f90e6",
+    "url": "/react-library/static/js/main.86cfaa37.chunk.js"
+  },
+  {
+    "revision": "3dbc65b51cf26ec56c5e",
+    "url": "/react-library/static/js/runtime-main.d7fa1cb6.js"
+  },
+  {
+    "revision": "8e96146bf58f0806c3b0f0190d5e5898",
+    "url": "/react-library/static/media/search.8e96146b.svg"
+  }
+]);
